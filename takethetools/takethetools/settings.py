044e9123c81ef1435ac73934324b52ba8f159702
stoertebeker23 <stoerte@posteo.net>
2021-03-10 11:32:32 +0100

    remove secret key and change to new

--- takethetools/takethetools/settings.py
+++ takethetools/takethetools/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-
+from .secrets import SECRET_KEY
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-ndm)%*8arh($l7gda8n*2kv==t)6bk%upazlh#66p&0$9wf#g'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
