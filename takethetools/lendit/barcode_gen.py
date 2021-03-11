88e8fa26f9b2034d994d7b7553d14a17079cab3a
stoertebeker23 <stoerte@posteo.net>
2021-03-11 10:05:45 +0100

    remove open cv, go with pillow, also resolve #13

--- takethetools/lendit/barcode_gen.py
+++ takethetools/lendit/barcode_gen.py
@@ -1,8 +1,8 @@
 from .models import Tool
 from barcode import EAN13
 from barcode.writer import ImageWriter
+from PIL import Image, ImageFont, ImageDraw
 import os
-import cv2
 import numpy as np
 class Sheet:
 
@@ -20,51 +20,47 @@ class Sheet:
         pages = 0
         stiched = None
         stiched_row = None
-        print(len(self.ids))
+
+        fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 20)
+
         for tool in self.ids:
+
             with open("barcodes/" + str(tool[0]) + ".png", 'wb') as f:
-                print(str(tool[0]))
-                print(EAN13())
                 EAN13(str(tool[0]), writer=ImageWriter()).write(f)
 
-            image = cv2.imread("barcodes/" + str(tool[0]) + ".png")
-            cv2.putText(image, tool[1] + " " + tool[2] + " von " + tool[3], (90, 220),
-                        cv2.FONT_HERSHEY_SIMPLEX, .6, (0, 0, 0), 2)
+            image = Image.open("barcodes/" + str(tool[0]) + ".png")
+
+            d_con = ImageDraw.Draw(image)
+            d_con.multiline_text((80,200), tool[1] + " " + tool[2], font=fnt,fill=(0, 0, 0))
+            d_con.multiline_text((80,225), tool[3] + " / " + tool[4], font=fnt, fill=(0, 0, 0))
+
             if x_images == 0:
-                print(x_images, y_images, "start")
                 stiched_row = image
                 x_images += 1
             elif x_images%4 != 0:
-                print(x_images, y_images,"add row")
                 stiched_row = np.concatenate((stiched_row, image), axis=1)
                 x_images += 1
             elif x_images%4 == 0 and x_images != 0:
-                print(x_images, y_images,"end row")
                 if y_images == 0:
-                    print("first row")
                     stiched = stiched_row
                     y_images += 1
 
-
                 elif y_images == 11:
-                    print(x_images, y_images,"page finished")
-                    cv2.imwrite("page" + str(pages) + ".png", stiched)
+                    im = Image.fromarray(stiched)
+                    im.save("page" + str(pages) + ".png","PNG")
                     y_images = 0
                 else:
-                    print(x_images, y_images,"next row")
                     stiched = np.concatenate((stiched, stiched_row), axis=0)
                     y_images += 1
                 stiched_row = None
                 x_images = 0
-
-            #cv2.imwrite("barcodes/" + str(tool[0]) + ".png", image)
 
         os.system("rm -r barcodes")
 
     def add_tool(self, id):
         tool = Tool.objects.get(id=id)
         for n in range(tool.available_amount):
-            self.ids.append([tool.id, tool.name, tool.model, tool.owner.username])
+            self.ids.append([tool.id, tool.name, tool.brand, tool.model, tool.owner.username])
 
     def list(self):
         for id in self.ids:
