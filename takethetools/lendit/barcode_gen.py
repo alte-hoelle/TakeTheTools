deef7a25ae9927d0a38d5cebc75ba74763c4b92e
stoertebeker23 <stoerte@posteo.net>
2021-03-13 16:16:47 +0100

    formatting of barcode sheets tweaked

--- takethetools/lendit/barcode_gen.py
+++ takethetools/lendit/barcode_gen.py
@@ -9,7 +9,7 @@ import wget
 
 class Sheet:
 
-    ids = []
+    barcodes = []
 
     def __str__(self):
         pass
@@ -23,32 +23,39 @@ class Sheet:
         stiched_row = None
 
         fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 20)
-
-        for tool in self.ids:
+        spacer_vert_width = 80
+        spacer_vert = Image.new('RGB', (spacer_vert_width,250), (255, 255, 255))
+        top_spacer  = Image.new('RGB', (4*523 + 4*spacer_vert_width, 25), (255, 255, 255))
+        print("Number: ", len(self.barcodes))
+        for tool in self.barcodes:
 
             with open("barcodes/" + str(tool[0]) + ".png", 'wb') as f:
                 EAN13(str(tool[0]), writer=ImageWriter()).write(f)
 
             image = Image.open("barcodes/" + str(tool[0]) + ".png")
+            image = image.crop((0,30,523,280))
 
             d_con = ImageDraw.Draw(image)
-            d_con.multiline_text((80,200), tool[1] + " " + tool[2], font=fnt,fill=(0, 0, 0))
-            d_con.multiline_text((80,225), tool[3] + " / " + tool[4], font=fnt, fill=(0, 0, 0))
+            d_con.multiline_text((80,165), tool[1] + " " + tool[2], font=fnt,fill=(0, 0, 0))
+            d_con.multiline_text((80,190), tool[3] + " / " + tool[4] + " GV", font=fnt, fill=(0, 0, 0))
 
             if x_images == 0:
-                stiched_row = image
+                stiched_row = np.concatenate((spacer_vert, image), axis=1)
+
                 x_images += 1
             elif x_images%4 != 0:
-                stiched_row = np.concatenate((stiched_row, image), axis=1)
+                stiched_row = np.concatenate((stiched_row, spacer_vert, image), axis=1)
                 x_images += 1
             elif x_images%4 == 0 and x_images != 0:
                 if y_images == 0:
                     stiched = stiched_row
                     y_images += 1
 
-                elif y_images == 11:
-                    im = Image.fromarray(stiched)
-                    im.save("page" + str(pages) + ".png","PNG")
+                elif y_images == 12:
+
+                    final = np.concatenate((top_spacer,stiched), axis = 0)
+                    im = Image.fromarray(final)
+                    im.save("page" + str(pages) + ".pdf", "PDF")
                     y_images = 0
                 else:
                     stiched = np.concatenate((stiched, stiched_row), axis=0)
@@ -58,14 +65,14 @@ class Sheet:
 
         os.system("rm -r barcodes")
 
-    def add_tool(self, id):
-        tool = Tool.objects.get(id=id)
+    def add_tool(self, barcode):
+        tool = Tool.objects.get(barcode_ean13_no_check_bit=barcode)
         for n in range(tool.available_amount):
-            self.ids.append([tool.id, tool.name, tool.brand, tool.model, tool.owner.username])
+            self.barcodes.append([tool.barcode_ean13_no_check_bit, tool.name, tool.brand, tool.model, tool.owner.username])
 
     def list(self):
-        for id in self.ids:
-            print(id)
+        for barcode in self.barcodes:
+            print(barcode)
 
 
 def fix_ids_to_EAN13():
