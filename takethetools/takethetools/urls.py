c67b3355e2f01e46b4dedca5b260e0ce8d2b32c5
stoertebeker23 <stoerte@posteo.net>
2021-04-09 13:20:58 +0200

    quickfix for merging

--- takethetools/takethetools/urls.py
+++ takethetools/takethetools/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path("/", include('lendit.urls')),
+    path("lendit/", include('lendit.urls')),
 ]
 
 if settings.DEBUG:
