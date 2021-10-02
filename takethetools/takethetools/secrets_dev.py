846f7e9c9de2a20d4c88b3ad43e2d22d618e671a
debauer <github@debauer.net>
2021-10-02 21:44:21 +0200

    seperated dev and prod environment quick and dirty

---
+++ takethetools/takethetools/secrets_dev.py
@@ -0,0 +1,2 @@
+SECRET_KEY = 'supersecretkey'
+CHIP_SALT = 'supersecretsalt'
--- takethetools/takethetools/settings.py
+++ takethetools/takethetools/settings.py
@@ -10,13 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-from .secrets import SECRET_KEY
 from pathlib import Path
-from .secrets import CHIP_SALT
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = True
+ENVIRONMENT = "dev"
+
+if ENVIRONMENT == "dev":
+    from .secrets_dev import SECRET_KEY
+    from .secrets_dev import CHIP_SALT
+else:
+    from .secrets_prod import SECRET_KEY
+    from .secrets_prod import CHIP_SALT
 
 # Application definition
 
@@ -72,7 +79,7 @@ WSGI_APPLICATION = 'takethetools.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': f"{BASE_DIR}/../database/{ENVIRONMENT}.sqlite3",
     }
 }
 
